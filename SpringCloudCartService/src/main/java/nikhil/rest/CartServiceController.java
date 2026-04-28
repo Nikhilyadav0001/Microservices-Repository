71cb2f874b4054ac178acef3dbd65aae3cda497f
Nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-04-28 12:21:09 +0530

    discovery client

--- SpringCloudCartService/src/main/java/nikhil/rest/CartServiceController.java
+++ SpringCloudCartService/src/main/java/nikhil/rest/CartServiceController.java
@@ -1,16 +1,29 @@
 package nikhil.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/api/product")
+@RequestMapping("/v1/api/cart")
 public class CartServiceController {
-
-    @GetMapping("/cart")
+	
+	@Value("${server.port}")
+	private String portNo;
+	
+	/*
+	 * METHOD  : GET
+	 * PATH    : /show
+	 * OUTPUT  : R.E<String>
+	 * URL     : /v1/api/cart/show
+	 * INPUT   : RequestHeader(TOKENID)
+	 */
+	
+	
+    @GetMapping("/show")
     public ResponseEntity<String> getProduct() {
-        return ResponseEntity.ok("getting the output of MS#1 Through EUREKA Server");
+        return ResponseEntity.ok("Welcome to Cart service Running on ::"+portNo);
     }
 }
