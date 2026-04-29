a3dc3f96fcc93f27069ddfa8791f0b5a971114e0
Nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-04-29 17:44:21 +0530

    Feign Client

--- SpringCloudCartService/src/main/java/nikhil/rest/CartServiceController.java
+++ SpringCloudCartService/src/main/java/nikhil/rest/CartServiceController.java
@@ -3,8 +3,13 @@ package nikhil.rest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import nikhil.response.Cart;
 
 @RestController
 @RequestMapping("/v1/api/cart")
@@ -23,7 +28,43 @@ public class CartServiceController {
 	
 	
     @GetMapping("/show")
-    public ResponseEntity<String> getProduct() {
+    public ResponseEntity<String> getCartDetails() {
         return ResponseEntity.ok("Welcome to Cart service Running on ::"+portNo);
     }
+    
+    /*
+	 * METHOD  : GET
+	 * PATH    : /find
+	 * OUTPUT  : R.E<Cart>
+	 * INPUT   : id (@Pathvariable)
+	 * URL     : /v1/api/cart/find/{id}
+	 * 
+	 */
+	
+	@GetMapping("/find/{id}")
+	public ResponseEntity<String> getCartById(
+				@PathVariable Integer id) {
+		Cart cart = new Cart();
+		cart.setCartId(id);
+		cart.setCartCost(32000.0);
+		cart.setCartCode("TEST");
+		
+		return ResponseEntity.ok(cart.toString()+"PORT OF CART:: "+portNo);
+	}
+	
+	/*
+	 * METHOD  : POST
+	 * PATH    : /create
+	 * OUTPUT  : R.E<String>
+	 * INPUT   : Cart (@Requestbody)
+	 * URL     : /v1/api/cart/create
+	 * 
+	 */
+	@PostMapping("/create")
+	public ResponseEntity<String> addToCart(
+			@RequestBody Cart cart) {
+		return ResponseEntity.ok("ADDED TO CART => " + cart);
+	}
+    
+    
 }
---
+++ SpringCloudOrderService-FeignClient/src/main/java/nikhil/rest/OrderRestController.java
@@ -0,0 +1,80 @@
+package nikhil.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import nikhil.consumer.ICartConsumer;
+import nikhil.response.Cart;
+
+@RestController
+@RequestMapping("/v1/api/order")
+public class OrderRestController {
+
+	@Value("${server.port}")
+	private String portNo;
+	@Autowired
+	private ICartConsumer consumer;
+	
+	/*
+	 * METHOD  : GET
+	 * PATH    : /place
+	 * OUTPUT  : R.E<String>
+	 * URL     : /v1/api/order/place
+	 * 
+	 */
+	
+	@GetMapping("/place")
+	public ResponseEntity<String> placeOrder() {
+		String cartResponse = consumer.getCartInfo().getBody();
+		return ResponseEntity.ok("ORDER PLACED WITH => "+cartResponse);
+	}
+	
+	/*
+	 * METHOD  : GET
+	 * PATH    : /fetch
+	 * OUTPUT  : R.E<String>
+	 * URL     : /v1/api/order/fetch
+	 * 
+	 */
+	@GetMapping("/fetch/{id}")
+	public ResponseEntity<String> fetchOrderWithCart(
+				@PathVariable Integer id
+			) 
+	{
+		String cob = null;
+		cob=consumer.getCartById(id).getBody();
+		return ResponseEntity.ok("PORT NO OF ORDER IS :: "+portNo +"   ORDER WITH CART DATA => " 
+				+ cob  );
+	}
+	
+	
+	/*
+	 * METHOD  : POST
+	 * PATH    : /addToCart
+	 * OUTPUT  : R.E<String>
+	 * URL     : /v1/api/order/addToCart
+	 * 
+	 */
+	@PostMapping("/addToCart")
+	public ResponseEntity<String> addToCart(
+			@RequestBody Cart cart
+			) 
+	{
+		String cartResp = null;
+		cartResp = consumer.addCartDetails(cart).getBody();
+		return ResponseEntity.ok("ORDER WITH => " + cartResp);
+	}
+	
+	
+	
+	
+	
+	
+}
