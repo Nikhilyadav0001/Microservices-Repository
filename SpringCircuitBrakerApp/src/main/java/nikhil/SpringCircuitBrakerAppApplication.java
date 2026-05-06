c1af27da2a4b0add0e976a519dee1df232439594
Nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-05-06 18:46:31 +0530

    Circuit braker

---
+++ SpringCircuitBrakerApp/src/main/java/nikhil/SpringCircuitBrakerAppApplication.java
@@ -0,0 +1,13 @@
+package nikhil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCircuitBrakerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCircuitBrakerAppApplication.class, args);
+	}
+
+}
